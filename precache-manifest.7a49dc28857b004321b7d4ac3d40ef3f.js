28f310c6d0a21b37dd1f794de468db01ce922e8b
Goodnews <ofogoodnews@gmail.com>
2023-02-12 22:34:22 +0100

    Updates

---
+++ precache-manifest.7a49dc28857b004321b7d4ac3d40ef3f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ed23020cfbab1f6fb755",
+    "url": "/my-react-app/static/css/main.3787c47b.chunk.css"
+  },
+  {
+    "revision": "ed23020cfbab1f6fb755",
+    "url": "/my-react-app/static/js/main.ed23020c.chunk.js"
+  },
+  {
+    "revision": "3e3c3eb55c09e01a1300",
+    "url": "/my-react-app/static/js/1.3e3c3eb5.chunk.js"
+  },
+  {
+    "revision": "cc6da4f6f6985093c8a0",
+    "url": "/my-react-app/static/js/2.cc6da4f6.chunk.js"
+  },
+  {
+    "revision": "410db56f78562ff7eba3",
+    "url": "/my-react-app/static/js/runtime~main.410db56f.js"
+  },
+  {
+    "revision": "f0856a9ad5f674b4e461e463f2359e87",
+    "url": "/my-react-app/index.html"
+  }
+];
